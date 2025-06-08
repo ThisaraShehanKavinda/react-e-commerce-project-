fe502ee2849dd38ed030dabbf2c06c25c6c3162d
Thisara Shehan Kavinda <138697372+ThisaraShehanKavinda@users.noreply.github.com>
2025-06-08 21:34:21 +0530

    styled header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,14 +3,14 @@ import menuIcon from '../../img/menu-svgrepo-com.svg'
 import searchIcon from '../../img/search-svgrepo-com.svg'
 
 const Header = () => {
-  return <Header classname = "w-full p-5 bg-my-background flex items-center justify-between">
+  return <Header classname = " drop-shadow-header-shadow fixed top-0 left-0 z-[100] sha w-full p-5 bg-my-background flex items-center justify-between">
     {/* header left */}
     <div className='flex items-center'>
         <img src={menuIcon} alt="Menu" classname = "w-6 h-6 object-contain cursor-pointer" />
         <h1 className=' ml-2 text-sm font-bold'>ShehanZ <span className='text-red-900'>Coding</span></h1>
-        <div className='flex items-center rounded-full bg-[#ffd4d4] '>
-            <input type="text" placeholder="Search" className='outline-none p-2 font-semibold w-[200px] bg-inherit' />
-            <img src={searchIcon} alt="Search" classname = "w-6 h-6 object-contain cursor-pointer" />
+        <div className=' overflow-hidden ml-2 flex items-center rounded-full bg-[#ffd4d4] '>
+            <input type="text" placeholder="Search" className='outline-none p-2 font-semibold w-[200px] bg-inherit ml-1' />
+            <img src={searchIcon} alt="Search" classname = "w-6 h-6 object-contain cursor-pointer mr-2" />
         </div>
     </div>
     {/* header right */}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
     extend: {
       colors:{
         'my-background': '#ffd9d9',
+      },
+      dropShadow:{
+        "header-shadow": 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
       }
     },
   },
